18d56e2f3216bc1d8ea9b7ee795f04af1e3c9ac2
beom84 <cjja00@konkuk.ac.kr>
2025-04-06 23:59:36 +0900

    백준_Gold_N-Queen_이승범

--- src/week1/백준_Gold_9663.kt
+++ src/week1/백준_Gold_9663.kt
@@ -3,47 +3,44 @@ package week1
 private val bw = System.`out`.bufferedWriter()
 private var n = 0
 private var count = 0
-private lateinit var visited: Array<Pair<Int, Int>>
+private lateinit var column: BooleanArray
+private lateinit var rightDiag: BooleanArray
+private lateinit var leftDiag: BooleanArray
 
 fun main() = with(System.`in`.bufferedReader()) {
     n = readLine().toInt()
 
-    visited = Array(n + 1) { 0 to 0 }
+    column = BooleanArray(n + 1)
+    rightDiag = BooleanArray(2 * n + 1)
+    leftDiag = BooleanArray(2 * n + 1)
+
     backTracking(1)
     bw.write("$count")
     bw.flush()
     bw.close()
 }
 
-fun backTracking(k: Int) {
-    if (k == n + 1) {
+fun backTracking(row: Int) {
+    if (row == n + 1) {
         count++
         return
     }
     for (i in 1..n) {
-        val location = k to i
-        if (!visited.contains(location) && checkVisited(location)) {
-            visited[k] = (location)
-            backTracking(k + 1)
-            visited[k] = 0 to 0
-        }
+        if (column[i] || rightDiag[row + i] || leftDiag[row - i + n]) continue
+
+        column[i] = true
+        rightDiag[row + i] = true
+        leftDiag[row - i + n] = true
+
+        backTracking(row + 1)
+
+        column[i] = false
+        rightDiag[row + i] = false
+        leftDiag[row - i + n] = false
+
     }
 }
 
-fun checkVisited(location: Pair<Int, Int>): Boolean {
-    val x = location.first
-    val y = location.second
-    for (i in visited.indices) {
-        val dx = visited[i].first
-        val dy = visited[i].second
-        if (dx == 0 && dy == 0) continue
-        if (x == dx) return false
-        if (y == dy) return false
-        if (x + y == dx + dy) return false
-        if (x - y == dx - dy) return false
-    }
-    return true
-}
 
 /*
     오른쪽 대각선은 x-y가 같으면 갈은곳이다
