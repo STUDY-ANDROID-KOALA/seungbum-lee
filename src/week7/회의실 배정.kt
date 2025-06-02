6d6ffa3a2d082c828d7303c85dbd1211ba63817b
beom84 <cjja00@konkuk.ac.kr>
2025-06-02 18:25:15 +0900

    백준_Gold_회의실 배정_이승범

--- src/week7/회의실 배정.kt
+++ src/week7/회의실 배정.kt
@@ -2,10 +2,8 @@ package week7
 
 private fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
+    val jobs = Array(n) {IntArray(2) { 0 } }
 
-    var jobs = Array(n) {IntArray(2) { 0 } }
-    var cnt = 0
-    var endTime = jobs[0][1];
     repeat(n) { idx ->
         val (start,end) = readLine().split(" ").map { it.toInt() }
         jobs[idx][0] = start
@@ -14,13 +12,14 @@ private fun main() = with(System.`in`.bufferedReader()) {
 
     jobs.sortWith(compareBy<IntArray>{ it[1]}.thenBy{ it[0] })
 
+    var cnt = 1
+    var endTime = jobs[0][1]
+
     for(i in 1 until jobs.size) { // n번 반복해서 모든 job을 비교
         if(endTime <= jobs[i][0]){
             endTime = jobs[i][1]
             cnt++
         }
     }
-
-
     println(cnt)
 }
